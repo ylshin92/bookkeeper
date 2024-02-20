b0c96205f78d0722675dbd4eeeabc9748e847e18
ylshin92 <ylshin92@gmail.com>
2024-02-19 20:13:02 -0500

    Initial commit

---
+++ src/main/java/com/example/bookkeeper/BookkeeperApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookkeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookkeeper/BookkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
